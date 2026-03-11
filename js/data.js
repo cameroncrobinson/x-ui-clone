2e01d31dd77d011f7e857d6137dca48e4037607c
Cameron Robinson <robinson.cameron90@gmail.com>
2026-03-11 14:25:18 -0400

    x-ui-1: initial commit

---
+++ js/data.js
@@ -0,0 +1,58 @@
+export const tweetsData = [
+  {
+    handle: `@bitcoinbro 💎`,
+    profilePic: `https://fastly.picsum.photos/id/419/200/300.jpg?hmac=jvSs1zyCZ3ATdTlvdfcTKBBGcrgnCk3EAvZt352Fbco`,
+    likes: 27,
+    retweets: 10,
+    tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices.
+            Guaranteed return on investment. HMU DMs open!!`,
+    replies: [],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+  },
+  {
+    handle: `@Selonk ✅`,
+    profilePic: `https://fastly.picsum.photos/id/419/200/300.jpg?hmac=jvSs1zyCZ3ATdTlvdfcTKBBGcrgnCk3EAvZt352Fbco`,
+    likes: 6500,
+    retweets: 234,
+    tweetText: `I need slaves, er, um, volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
+    replies: [
+      {
+        handle: `@Tanjiro`,
+        profilePic: `https://fastly.picsum.photos/id/419/200/300.jpg?hmac=jvSs1zyCZ3ATdTlvdfcTKBBGcrgnCk3EAvZt352Fbco`,
+        tweetText: `AKUZA!`,
+      },
+      {
+        handle: `@Donnie ✅`,
+        profilePic: `https://fastly.picsum.photos/id/419/200/300.jpg?hmac=jvSs1zyCZ3ATdTlvdfcTKBBGcrgnCk3EAvZt352Fbco`,
+        tweetText: `Will there be free uranium?`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+  },
+  {
+    handle: `@Yor`,
+    profilePic: `https://fastly.picsum.photos/id/419/200/300.jpg?hmac=jvSs1zyCZ3ATdTlvdfcTKBBGcrgnCk3EAvZt352Fbco`,
+    likes: 10,
+    retweets: 3,
+    tweetText: `My daughter is so cute!`,
+    replies: [
+      {
+        handle: `@Frankie ☣️`,
+        profilePic: `https://fastly.picsum.photos/id/419/200/300.jpg?hmac=jvSs1zyCZ3ATdTlvdfcTKBBGcrgnCk3EAvZt352Fbco`,
+        tweetText: `I live little Anya.`,
+      },
+      {
+        handle: `@Loid`,
+        profilePic: `https://fastly.picsum.photos/id/419/200/300.jpg?hmac=jvSs1zyCZ3ATdTlvdfcTKBBGcrgnCk3EAvZt352Fbco`,
+        tweetText: `She is of adequate cuteness❤️`,
+      },
+    ],
+    isLiked: false,
+    isRetweeted: false,
+    uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+  },
+]
